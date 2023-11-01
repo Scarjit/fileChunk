8ad8a000b629df373fa21372b858d96f689daeb1
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2023-11-01 14:54:15 +0100

    fix: correctly restore duplicates

--- src/chunkstream.rs
+++ src/chunkstream.rs
@@ -23,6 +23,8 @@ pub(crate) struct ChunkBase {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct RestoreInformation {
     files: HashMap<String, IndexMap<String, StartEndTuple>>,
+    hashes: HashMap<String, String>,
+    duplicates: HashMap<String, Vec<String>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -67,29 +69,29 @@ impl Chunk {
 
     pub(crate) fn add_file(&mut self, file: &str, bytes: &Vec<u8>) -> Vec<u8> {
         let mut vec_dq_bytes = VecDeque::from(bytes.to_vec());
-        let mut written = 0;
+        let mut written: u64 = 0;
         for _ in 0..bytes.len() {
             let byte = vec_dq_bytes.pop_front().unwrap();
             self.buffer.push(byte);
             self.base.fingerprint.push_byte(byte);
             written += 1;
-            if self.base.fingerprint.value() % CHUNK_MODULUS == 0 {
-                self.current_offset += written as u64;
+            if self.base.fingerprint.value() % CHUNK_MODULUS == 0{
+                self.current_offset += written;
                 self.base.files.push(ChunkFile {
                     filename: file.to_string(),
                     name: self.base.fingerprint.value().to_string(),
-                    start: self.current_offset - written as u64,
+                    start: self.current_offset - written,
                     end: self.current_offset,
                 });
                 return vec_dq_bytes.make_contiguous().to_vec();
             }
         }
-        self.current_offset += written as u64;
+        self.current_offset += written;
         println!("File: {}, written: {}, remaining: {}, fingerprint: {}", file, written, vec_dq_bytes.len(), self.base.fingerprint.value());
         self.base.files.push(ChunkFile {
             filename: file.to_string(),
             name: self.base.fingerprint.value().to_string(),
-            start: self.current_offset - written as u64,
+            start: self.current_offset - written,
             end: self.current_offset,
         });
 
@@ -105,24 +107,24 @@ impl Chunk {
         }
         let mut file = fs::File::create(path).unwrap();
         // Snappy compress
-        /*
         snap::write::FrameEncoder::new(&mut file)
             .write_all(&self.buffer)
             .unwrap();
-
-         */
-        file.write_all(&self.buffer).unwrap();
     }
 }
 
 pub struct Chunker {
     bases: HashMap<String, Vec<ChunkBase>>,
+    hash_to_path_map: HashMap<String, Vec<String>>,
+    path_to_hash_map: HashMap<String, String>,
 }
 
 impl Chunker {
     pub(crate) fn new() -> Chunker {
         Chunker {
             bases: HashMap::new(),
+            hash_to_path_map: HashMap::new(),
+            path_to_hash_map: HashMap::new(),
         }
     }
     pub(crate) fn add_files(mut self, mut paths: Vec<String>, output_path: &str) {
@@ -136,6 +138,24 @@ impl Chunker {
             let path = path.replace('\\', "/");
             // Try read file
             remaining_bytes = fs::read(&path).expect("Unable to read file");
+            let file_hash_blake = blake3::hash(&remaining_bytes).to_hex().to_ascii_lowercase();
+            match self.hash_to_path_map.get(&file_hash_blake) {
+                None => {
+                    self.hash_to_path_map.insert(file_hash_blake.clone(), vec![path.clone()]);
+                    self.path_to_hash_map.insert(path.clone(), file_hash_blake.clone());
+                }
+                Some(h) => {
+                    let mut h = h.clone();
+                    h.push(path.clone());
+                    self.hash_to_path_map.insert(file_hash_blake.clone(), h);
+                    self.path_to_hash_map.insert(path.clone(), file_hash_blake.clone());
+                    // We can skip processing now, since we already have this file
+                    println!("Skipping file: {}", path);
+                    continue;
+                }
+            }
+
+
             while !remaining_bytes.is_empty() {
                 remaining_bytes = chunk.add_file(&path, &remaining_bytes);
                 if chunk.base.fingerprint.value() % CHUNK_MODULUS == 0 {
@@ -204,6 +224,8 @@ impl Chunker {
 
         let mut restore_info = RestoreInformation {
             files: HashMap::new(),
+            hashes: HashMap::new(),
+            duplicates: HashMap::new(),
         };
 
 
@@ -228,6 +250,9 @@ impl Chunker {
             }
             restore_info.files.insert(filename.to_string(), file_map);
         }
+        restore_info.hashes = self.path_to_hash_map.clone();
+        // Filter out all keys, where there value is only len 1
+        restore_info.duplicates = self.hash_to_path_map.clone().into_iter().filter(|(_, v)| v.len() > 1).collect();
 
         let yaml = serde_yaml::to_string(&restore_info).unwrap();
         file.write_all(yaml.as_bytes()).unwrap();
@@ -252,18 +277,27 @@ impl Chunker {
         println!("Restoring: {}", filename);
         println!("{:?}", restore_info);
 
-        let file_map = restore_info.files.get(&filename).unwrap();
+        let file_map = match restore_info.files.get(&filename) {
+            None => {
+                // Check if it is a duplicate file
+                let key = restore_info.hashes.get(&filename).unwrap();
+                let ri_key = restore_info.duplicates.get(key).unwrap().first().unwrap();
+                restore_info.files.get(ri_key).unwrap()
+            }
+            Some(v) => {
+                v
+            }
+        };
+
         for (chunk_name, start_end) in file_map.iter() {
             let chunk_path = format!("{}/{}.chunk", data_path, chunk_name);
             println!("Chunk path: {}", chunk_path);
             let chunk_bytes = fs::read(chunk_path).unwrap();
             // Snappy decompress
-            /*
             let chunk_bytes = snap::read::FrameDecoder::new(&chunk_bytes[..])
                 .bytes()
                 .map(|x| x.unwrap())
                 .collect::<Vec<u8>>();
-*/
             let chunk_bytes =
                 chunk_bytes[start_end.start as usize..start_end.end as usize].to_vec();
             println!("Chunk: {}", chunk_name);
